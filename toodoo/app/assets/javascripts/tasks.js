7c668fe7eae3c97ce3fa93714cb392d107ba83a1
zannain <zannainshafi@gmail.com>
2017-08-15 18:37:18 -0400

    updated with apps made until  week 7

---
+++ toodoo/app/assets/javascripts/tasks.js
@@ -0,0 +1,10 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function() {
+  console.log('document is ready', new Date());
+  $("#task_name").focus();
+
+$(".modal-footer .btn-primary").click(function () {
+    $('.modal-body form').submit();
+  });
+});
