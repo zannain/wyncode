e6b39281dfd5696e00d92c2b19a2661d63c9e196
zannain <zannainshafi@gmail.com>
2017-08-17 11:33:43 -0400

    created login

--- secretnotes/app/controllers/users_controller.rb
+++ secretnotes/app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+
+    return render action: 'new' unless @user.save
+
+    redirect_to root_path, notice: 'Created user'
 end
+
+private
+
+def user_params
+  params.
+    require(:user).
+    permit(:username,
+          :password,
+          :password_confirmation)
+end
+end
+
